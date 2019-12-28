8fb281285727e61ff3fc7a2999fe943b4b224a83
lumiao <LuMisun@126.com>
2019-12-28 13:00:28 +0800

    red testMultipleOf5

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -6,4 +6,9 @@ public class FizzBuzzTest {
     public void should_return_Fizz_given_multiple_of_3() {
         Assert.assertEquals("Fizz", FizzBuzz.of(3));
     }
+
+    @Test
+    public void should_return_Fizz_given_multiple_of_5() {
+        Assert.assertEquals("Buzz", FizzBuzz.of(5));
+    }
 }
