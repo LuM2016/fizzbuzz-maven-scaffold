331954a09612242d4490238b2dbc584146713c38
lumiao <LuMisun@126.com>
2019-12-28 12:59:49 +0800

    pass testMultipleOf3

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,5 +1,10 @@
 public class FizzBuzz {
+    private static final int FIZZ_NUMBER = 3;
+
     public static String of(int number) {
+        if (number % FIZZ_NUMBER == 0) {
+            return "Fizz";
+        }
         return null;
     }
 }
