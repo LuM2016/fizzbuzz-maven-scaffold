b77e8d609d31a369a7e5ff9794a075bff55c05b5
lumiao <LuMisun@126.com>
2019-12-28 13:01:28 +0800

    pass testMultipleOf5

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,9 +1,13 @@
 public class FizzBuzz {
     private static final int FIZZ_NUMBER = 3;
+    private static final int BUZZ_NUMBER = 5;
 
     public static String of(int number) {
         if (number % FIZZ_NUMBER == 0) {
             return "Fizz";
+        }
+        if (number % BUZZ_NUMBER == 0) {
+            return "Buzz";
         }
         return null;
     }
